8a1e169055e85e35d15cc74352c1dc00afa538dd
charles <charlesmiranda@inforgeneses.com.br>
2017-08-17 13:56:08 -0300

    pastas css e js

--- application/template/admin/footer.php
+++ application/template/admin/footer.php
@@ -2,7 +2,7 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 2.4.0
     </div>
-    <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+    <strong>Copyright &copy; 2017-<?=date('Y')?> <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
     reserved.
   </footer>
 
@@ -199,43 +199,43 @@
   <!-- Add the sidebar's background. This div must be placed
        immediately after the control sidebar -->
   <div class="control-sidebar-bg"></div>
-</div>
+
 <!-- ./wrapper -->
 
 <!-- jQuery 3 -->
-<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<script src="<?=asset_url($template)?>js/jquery.min.js"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="bower_components/jquery-ui/jquery-ui.min.js"></script>
+<script src="<?=asset_url($template)?>js/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button);
 </script>
 <!-- Bootstrap 3.3.7 -->
-<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="<?=asset_url($template)?>js/bootstrap.min.js"></script>
 <!-- Morris.js charts -->
-<script src="bower_components/raphael/raphael.min.js"></script>
-<script src="bower_components/morris.js/morris.min.js"></script>
+<script src="<?=asset_url($template)?>js/raphael.min.js"></script>
+<script src="<?=asset_url($template)?>js/morris.min.js"></script>
 <!-- Sparkline -->
-<script src="bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+<script src="<?=asset_url($template)?>js/jquery.sparkline.min.js"></script>
 <!-- jvectormap -->
-<script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+<script src="<?=asset_url($template)?>js/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="<?=asset_url($template)?>js/jquery-jvectormap-world-mill-en.js"></script>
 <!-- jQuery Knob Chart -->
-<script src="bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
+<script src="<?=asset_url($template)?>js/jquery.knob.min.js"></script>
 <!-- daterangepicker -->
-<script src="bower_components/moment/min/moment.min.js"></script>
-<script src="bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
+<script src="<?=asset_url($template)?>js/moment.min.js"></script>
+<script src="<?=asset_url($template)?>js/daterangepicker.js"></script>
 <!-- datepicker -->
-<script src="bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+<script src="<?=asset_url($template)?>js/bootstrap-datepicker.min.js"></script>
 <!-- Bootstrap WYSIHTML5 -->
-<script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+<script src="<?=asset_url($template)?>js/bootstrap3-wysihtml5.all.min.js"></script>
 <!-- Slimscroll -->
-<script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<script src="<?=asset_url($template)?>js/jquery.slimscroll.min.js"></script>
 <!-- FastClick -->
-<script src="bower_components/fastclick/lib/fastclick.js"></script>
+<script src="<?=asset_url($template)?>js/fastclick.js"></script>
 <!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
+<script src="<?=asset_url($template)?>js/adminlte.min.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="dist/js/pages/dashboard.js"></script>
+<script src="<?=asset_url($template)?>js/dashboard.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="dist/js/demo.js"></script>
+<script src="<?=asset_url($template)?>js/demo.js"></script>
--- application/template/site/head.php
+++ application/template/site/head.php
@@ -11,35 +11,35 @@
     
     <!-- Favicons
     ================================================== -->
-    <link rel="shortcut icon" href="<?=IMAGENS?>favicon.ico" type="image/x-icon">
-    <link rel="apple-touch-icon" href="<?=IMAGENS?>apple-touch-icon.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="<?=IMAGENS?>apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="<?=IMAGENS?>apple-touch-icon-114x114.png">
+    <link rel="shortcut icon" href="<?=asset_url()?>imagens/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="<?=asset_url()?>imagens/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?=asset_url()?>imagens/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?=asset_url()?>imagens/apple-touch-icon-114x114.png">
 
     <!-- Bootstrap -->
-    <link rel="stylesheet" type="text/css"  href="<?=asset_url()?>css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="<?=FONTS?>font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css"  href="<?=asset_url($template)?>css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="<?=asset_url($template)?>fonts/font-awesome/css/font-awesome.css">
 
     <!-- Nivo Lightbox
     ================================================== -->
-    <link rel="stylesheet" href="<?=asset_url()?>css/nivo-lightbox.css" >
-    <link rel="stylesheet" href="<?=asset_url()?>css/nivo_lightbox_themes/default/default.css">
+    <link rel="stylesheet" href="<?=asset_url($template)?>css/nivo-lightbox.css" >
+    <link rel="stylesheet" href="<?=asset_url($template)?>css/nivo_lightbox_themes/default/default.css">
 
     <!-- Slider
     ================================================== -->
-    <link href="<?=asset_url()?>css/owl.carousel.css" rel="stylesheet" media="screen">
-    <link href="<?=asset_url()?>css/owl.theme.css" rel="stylesheet" media="screen">
+    <link href="<?=asset_url($template)?>css/owl.carousel.css" rel="stylesheet" media="screen">
+    <link href="<?=asset_url($template)?>css/owl.theme.css" rel="stylesheet" media="screen">
 
     <!-- Stylesheet
     ================================================== -->
-    <link rel="stylesheet" type="text/css"  href="<?=asset_url()?>css/style.css">
-    <link rel="stylesheet" type="text/css" href="<?=asset_url()?>css/responsive.css">
+    <link rel="stylesheet" type="text/css"  href="<?=asset_url($template)?>css/style.css">
+    <link rel="stylesheet" type="text/css" href="<?=asset_url($template)?>css/responsive.css">
 
     <!-- Google Fonts
     ================================================== -->
     <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
 
-    <script type="text/javascript" src="<?=JS?>modernizr.custom.js"></script>
+    <script type="text/javascript" src="<?=asset_url($template)?>js/modernizr.custom.js"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
